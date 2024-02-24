a132d5783331e7544f26186e74c6fb07cbf0a15c
Renzo Zuñiga <renzo.zuniga@pucp.edu.pe>
2024-02-24 16:36:24 -0500

    Add cors extensions

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
 import countryRoutes from "./routes/country.routes";
 
 // Usamos express para poder crear un servidor web que maneje rutas (GET, POST, PUT, DELETE)
@@ -11,6 +12,15 @@ app.set("port", 4000);
 // Middlewares
 // Usamos morgan en modo de desarrollo para tener un detalle de las peticiones en la consola
 app.use(morgan("dev"));
+
+// CORS
+// Usamos cors para pasar cookies u otras credenciales a cliente
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
 // Routes
 app.use("/api/countries", countryRoutes);
